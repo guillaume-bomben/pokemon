fc87042c1d1f74905c253478d6f7665b6f788895
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-01-10 10:33:55 +0100

    menu up

--- combat.py
+++ combat.py
@@ -20,19 +20,23 @@ class Combat:
         self.position_joueur = (200, 300)  # Position du joueur
         self.position_adversaire = (550, 100)  # Position de l'adversaire
         
-        self.vie_joueur = 100  # Santé du joueur
-        self.vie_adversaire = 100  # Santé de l'adversaire
-        self.couleur_vie = (0, 255, 0)  # Couleur de la barre de vie (verte)
-        self.tour_joueur = True  # Pour suivre le tour du joueur
-        
-        # Coordonnées du menu
+        self.vie_joueur = 100
+        self.vie_adversaire = 100
+        self.couleur_vie = (0, 255, 0)
+
+        self.tour_joueur = True
+        self.afficher_menu = True
+
         self.menu_pos_x = 200
         self.menu_pos_y = 500
         self.menu_largeur = 400
         self.menu_hauteur = 80
 
-        # Coordonnées et dimensions des boutons dans le menu
         self.bouton_attaque_pos = (self.menu_pos_x + 20, self.menu_pos_y + 20, 100, 40)
+        self.bouton_1_pos = (self.menu_pos_x + 20, self.menu_pos_y + 20, 100, 40)
+        self.bouton_2_pos = (self.menu_pos_x + 140, self.menu_pos_y + 20, 100, 40)
+        self.bouton_3_pos = (self.menu_pos_x + 260, self.menu_pos_y + 20, 100, 40)
+        self.bouton_4_pos = (self.menu_pos_x + 380, self.menu_pos_y + 20, 100, 40)
 
 
     def afficher(self):
@@ -48,21 +52,30 @@ class Combat:
         pygame.draw.rect(ecran, self.couleur_vie, (self.position_adversaire[0], self.position_adversaire[1] - 20, self.vie_adversaire, 10))
         pygame.draw.rect(ecran, noir, (self.position_adversaire[0], self.position_adversaire[1] - 20, 100, 10), 2)  # Bordure
         
-        # Dessiner le menu
-        pygame.draw.rect(ecran, blanc, (self.menu_pos_x, self.menu_pos_y, self.menu_largeur, self.menu_hauteur))
-        pygame.draw.rect(ecran, noir, (self.menu_pos_x, self.menu_pos_y, self.menu_largeur, self.menu_hauteur), 2)
-
-        # Dessiner les boutons du menu
-        pygame.draw.rect(ecran, noir, self.bouton_attaque_pos)
-        font = pygame.font.SysFont(None, 30)
-        text_attaque = font.render("Attaquer", True, noir)
-        ecran.blit(text_attaque, (self.bouton_attaque_pos[0] + 10, self.bouton_attaque_pos[1] + 10))
+        if self.afficher_menu:
+            pygame.draw.rect(ecran, noir, self.bouton_attaque_pos)
+            font = pygame.font.SysFont(None, 30)
+            text_attaque = font.render("Attaquer", True, noir)
+            ecran.blit(text_attaque, (self.bouton_attaque_pos[0] + 10, self.bouton_attaque_pos[1] + 10))
+        else:
+            pygame.draw.rect(ecran, noir, self.bouton_1_pos)
+            pygame.draw.rect(ecran, noir, self.bouton_2_pos)
+            pygame.draw.rect(ecran, noir, self.bouton_3_pos)
+            pygame.draw.rect(ecran, noir, self.bouton_4_pos)
 
     def attaquer(self):
+        '''
         if self.tour_joueur:
             self.vie_adversaire -= 20  # Réduire les points de vie de l'adversaire lors de l'attaque du joueur
             self.tour_joueur = False  # Passer au tour de l'adversaire
             self.attaquer_adversaire()
+        '''
+        if self.tour_joueur:
+            self.vie_adversaire -= 20
+            self.tour_joueur = False
+            self.afficher_menu = False
+
+    
     def attaquer_adversaire(self):
         # Simuler une attaque de l'adversaire (pour l'exemple, cela réduit de manière aléatoire la vie du joueur)
         if self.vie_joueur > 0:  # Vérifier si le joueur est toujours en vie
@@ -73,10 +86,11 @@ class Combat:
     def gerer_evenements(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            # Vérifier si le clic est effectué sur le bouton d'attaque
-            if self.bouton_attaque_pos[0] <= mouse_x <= self.bouton_attaque_pos[0] + self.bouton_attaque_pos[2] \
-                    and self.bouton_attaque_pos[1] <= mouse_y <= self.bouton_attaque_pos[1] + self.bouton_attaque_pos[3]:
-                self.attaquer()  # Appeler la méthode d'attaque
+            if self.afficher_menu:
+                if self.bouton_attaque_pos[0] <= mouse_x <= self.bouton_attaque_pos[0] + self.bouton_attaque_pos[2] \
+                        and self.bouton_attaque_pos[1] <= mouse_y <= self.bouton_attaque_pos[1] + self.bouton_attaque_pos[3]:
+                    self.attaquer()
+
 
     def update(self):
         # Mettre à jour l'état du jeu
