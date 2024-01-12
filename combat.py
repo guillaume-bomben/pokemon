67311fec51cc03c48f45d6e014a37566cf3e7389
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-01-12 10:08:32 +0100

    clean

--- combat.py
+++ combat.py
@@ -43,17 +43,6 @@ class Combat:
         self.tour_joueur = True
         self.afficher_menu = True
 
-        self.menu_pos_x = 200
-        self.menu_pos_y = 500
-        self.menu_largeur = 400
-        self.menu_hauteur = 80
-
-        self.bouton_attaque_pos = (self.menu_pos_x + 20, self.menu_pos_y + 20, 100, 40)
-        self.bouton_1_pos = (self.menu_pos_x + 20, self.menu_pos_y + 20, 100, 40)
-        self.bouton_2_pos = (self.menu_pos_x + 140, self.menu_pos_y + 20, 100, 40)
-        self.bouton_3_pos = (self.menu_pos_x + 260, self.menu_pos_y + 20, 100, 40)
-        self.bouton_4_pos = (self.menu_pos_x + 380, self.menu_pos_y + 20, 100, 40)
-
 
     def afficher(self):
         ecran.blit(self.fond, (0, 0))  # Afficher le fond du combat
@@ -68,24 +57,8 @@ class Combat:
         pygame.draw.rect(ecran, self.couleur_vie, (131,140, 168, 12))
         pygame.draw.rect(ecran, noir, (131, 140, 168, 12), 2)  # Bordure
         
-        if self.afficher_menu:
-            pygame.draw.rect(ecran, noir, self.bouton_attaque_pos)
-            font = pygame.font.SysFont(None, 30)
-            text_attaque = font.render("Attaquer", True, noir)
-            ecran.blit(text_attaque, (self.bouton_attaque_pos[0] + 10, self.bouton_attaque_pos[1] + 10))
-        else:
-            pygame.draw.rect(ecran, noir, self.bouton_1_pos)
-            pygame.draw.rect(ecran, noir, self.bouton_2_pos)
-            pygame.draw.rect(ecran, noir, self.bouton_3_pos)
-            pygame.draw.rect(ecran, noir, self.bouton_4_pos)
 
     def attaquer(self):
-        '''
-        if self.tour_joueur:
-            self.vie_adversaire -= 20  # Réduire les points de vie de l'adversaire lors de l'attaque du joueur
-            self.tour_joueur = False  # Passer au tour de l'adversaire
-            self.attaquer_adversaire()
-        '''
         if self.tour_joueur:
             self.ennemy.ppv = int(self.ennemy.ppv)
             self.ennemy.ppv -= 20
