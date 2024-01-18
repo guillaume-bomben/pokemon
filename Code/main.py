ded08f4a460da13bc923b2d293f919d83987a996
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-01-18 13:54:54 +0100

    ajout sprite et attaque pokemon

--- Code/main.py
+++ Code/main.py
@@ -6,7 +6,7 @@ pygame.init()
 blanc = (255, 255, 255)
 
 def main():
-    combat = Combat("Amonistar","Amonistar")
+    combat = Combat("Tortank","Tortank")
 
 if __name__ == "__main__":
     main()
