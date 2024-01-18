f3bfd9c445f6ab89f46c66066d23cd40a8d117ca
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-01-18 15:00:41 +0100

    add sprite and atk pokemon

--- Code/main.py
+++ Code/main.py
@@ -6,7 +6,7 @@ pygame.init()
 blanc = (255, 255, 255)
 
 def main():
-    combat = Combat("Tortank","Tortank")
+    combat = Combat("Papilusion","Papilusion")
 
 if __name__ == "__main__":
     main()
