4f722e2ea0ff0b8f95f247500101553005a53a54
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-01-23 13:30:06 +0100

    push

--- Code/menu.py
+++ Code/menu.py
@@ -3,27 +3,52 @@ import pygame
 class Menu:
     def __init__(self):
         self.background = pygame.image.load("assets/Menu/Background menu.png")
+
+        original_image = pygame.image.load("assets/Menu/2.png")
+        self.m2_size = (220, 36)
+        self.m2 = pygame.transform.scale(original_image, self.m2_size)
+        self.m2_hover = pygame.transform.scale(original_image, (int(self.m2_size[0]*1.2), int(self.m2_size[1]*1.2)))  # Image agrandie pour la souris
+
         self.ecran = pygame.display.set_mode((800, 600))
         self.font = pygame.font.Font(None, 36)
+        self.is_hovered = [False, False, False]  # Un tableau pour stocker l'état de survol des images
         running = True
         while running:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEMOTION:
+                    self.gerer_survol(event.pos)
             self.afficher()
-            pygame.display.flip() 
+            pygame.display.flip()
+
+        pygame.quit()
         
         
+    def gerer_survol(self, mouse_pos):
+        # Vérifiez si la souris survole l'image
+        for i in range(3):
+            image_rect = self.m2.get_rect(center=(400, 300 + i * 100))
+            self.is_hovered[i] = image_rect.collidepoint(mouse_pos)
+
     def afficher(self):
-        self.ecran.blit(self.background,(0,0))
-        
-        b1 = self.font.render("menu 1",True,(255,255,255))
-        b1_pos = b1.get_rect(midbottom=(400,300))
-        self.ecran.blit(b1,b1_pos)
-        b2 = self.font.render("menu 2",True,(255,255,255))
-        b2_pos = b2.get_rect(midbottom=(400,400))
-        self.ecran.blit(b2,b2_pos)
-        b3 = self.font.render("menu 3",True,(255,255,255))
-        b3_pos = b3.get_rect(midbottom=(400,500))
-        self.ecran.blit(b3,b3_pos)
+        self.ecran.blit(self.background, (0, 0))
+
+        for i in range(3):
+            if self.is_hovered[i]:
+                self.ecran.blit(self.m2_hover, self.m2_hover.get_rect(center=(400, 300 + i * 100)))
+            else:
+                self.ecran.blit(self.m2, self.m2.get_rect(center=(400, 300 + i * 100)))
+
+        b1 = self.font.render("Jouer", True, (255, 255, 255))
+        b1_pos = b1.get_rect(center=(400, 300))
+        self.ecran.blit(b1, b1_pos)
+
+        b2 = self.font.render("Add Pokemon", True, (255, 255, 255))
+        b2_pos = b2.get_rect(center=(400, 400))
+        self.ecran.blit(b2, b2_pos)
+
+        b3 = self.font.render("Pokedex", True, (255, 255, 255))
+        b3_pos = b3.get_rect(center=(400, 500))
+        self.ecran.blit(b3, b3_pos)
---
+++ Code/pokedex.py
@@ -0,0 +1,7 @@
+import pygame
+
+class pokedex:
+    def __init__(self):
+        self.ecran = pygame.display.set_mode((800, 600))
+        
+        
